ab4081629159560f1d34c1577dd9b350c6ffb30f
M. Corey Glass <dhexcxr@gmail.com>
2022-08-31 11:30:11 -0400

    feat: build To Do List item

---
+++ lister/ListItem.java
@@ -0,0 +1,53 @@
+package lister;
+
+import java.util.Date;
+
+public class ListItem {
+	
+	private String toDoItem;
+	private Date creationDateTime;
+	private boolean done;
+	private Date finishedDateTime;
+	
+	public String getToDoItem() {
+		return toDoItem;
+	}
+
+	public void setToDoItem(String toDoItem) {
+		this.toDoItem = toDoItem;
+	}
+
+	public Date getCreationDateTime() {
+		return creationDateTime;
+	}
+
+	public void setCreationDateTime(Date creationDateTime) {
+		this.creationDateTime = creationDateTime;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	public Date getFinishedDateTime() {
+		return finishedDateTime;
+	}
+
+	public void setFinishedDateTime(Date finishedDateTime) {
+		this.finishedDateTime = finishedDateTime;
+	}
+	
+	public ListItem(String toDoItem) {
+		setToDoItem(toDoItem);
+		setCreationDateTime(new Date(System.currentTimeMillis()));
+		setDone(false);
+		setFinishedDateTime(new Date());
+	}
+	
+	
+
+}
