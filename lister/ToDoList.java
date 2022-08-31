525e4bc6cea0db9f5b32a10abcd73ce4a2ddbc83
M. Corey Glass <dhexcxr@gmail.com>
2022-08-31 12:16:49 -0400

    feat: make ToDoList print its own name

--- lister/ToDoList.java
+++ lister/ToDoList.java
@@ -1,5 +1,7 @@
 package lister;
 
+import static java.lang.System.out;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,18 +13,35 @@ public class ToDoList {
 	public String getListName() {
 		return listName;
 	}
+	
 	public void setListName(String listName) {
 		this.listName = listName;
 	}
+	
 	public List<ListItem> getListItems() {
 		return listItems;
 	}
+	
+	public void showListItems() {
+		// TODO print list items
+	}
+	
 	public void setListItems(List<ListItem> listItems) {
 		this.listItems = listItems;
 	}
 	
 	public ToDoList(String listName) {
 		setListName(listName);
-		setListItems(new ArrayList<>());;
+		setListItems(new ArrayList<>());
+	}
+	
+	public void openList() {
+		// TODO print menu of List Options
+	}
+	
+	@Override
+	public String toString() {
+		// TODO make better
+		return "ToDoList [listName=" + listName + "]";
 	}
 }
